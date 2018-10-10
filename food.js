f4ea59004b21ad37729a80a88177fb0a681d8a3e
[abigaile] <abigailelewis57@gmail.com>
2018-10-10 19:52:26 -0400

    api

--- food.js
+++ food.js
@@ -1,6 +1,6 @@
-const baseURL ="https://api.edamam.com/api/food-database/parser";
-const app_id = "f70677f6";
-const app_key = "b7460474033829fd16a4927f735e570e";
+const baseURL = "https://api.edamam.com/search?q="//cheese&app_id=d1fd634c&app_key=eae9a525ecd75f348130b08de5e95920";
+const app_id = "d1fd634c";
+const app_key = "eae9a525ecd75f348130b08de5e95920";
 let url;
 
 
@@ -11,56 +11,102 @@ let url;
 //let calories = document.getElementById('calories').value;
 const searchTerm = document.getElementById('search');
 const searchForm = document.getElementById('formId');
-const nav = document.querySelector('nav');
+//const nav = document.querySelector('nav');
 const section = document.querySelector('section');
+//const maxCal = document.getElementById('maxCal');
 
 
-searchForm.addEventListener('submit', fetchResults); 
-function fetchResults(e) { 
-   e.preventDefault();
-    url = `${baseURL}?app_id=${app_id}&app_key=${app_key}&ingr=${encodeURIComponent(search.value)}`;  //&q=${search.value}`;   //'https://api.edamam.com/api/nutrition-details &nutrients=${nutrients}
+searchForm.addEventListener('submit', fetchResults);
+function fetchResults(e) {
+    e.preventDefault();
+    url = `${baseURL}${searchTerm.value}&from=0&to=2&app_id=${app_id}&app_key=${app_key}`;  //&q=${search.value}`;   //
+    // url = baseURL;
     console.log('URL:', url);
-   //if(search.value !== ''){
-     //   console.log(search.value)
-       // url += '&search_term=' + search.value;
-    
+    //if(search.value !== ''){
+    //   console.log(search.value)
+    // url += '&search_term=' + search.value;
+
     //};
 
     fetch(url)
-    .then(function(result) {
-      console.log(result)
-      return result.json(); 
-    })
-    .then(function(json) {
-      console.log(json);
-      displayResults(json);
-    })
+        .then(function (result) {
+            console.log(result)
+            return result.json();
+        })
+        .then(function (json) {
+            console.log(json);
+            displayResults(json);
+
+        })
 };
-//let label = document.createElement('label');
 
 function displayResults(json) {
     while (section.firstChild) {
         section.removeChild(section.firstChild);
-      } console.log(json);
+    }
+    console.log(json);
 
-      
 
-        if (label.length === 0){
-            console.log("no results")
-        } else {
-        for( let i = 0; i < label.length; i++){
+    console.log(json.hits.length);
+    if (json.hits === 0) {
+        console.log("no results");
+
+    } else {
+        for (let i = 0; i < json.hits.length; i++) {
+            console.log("starting loop");
+            let article = document.createElement('article');
+
             let para1 = document.createElement('p');
-            let label = json.hints[i].food.label;
-            para1.setAttribute("class",label);
-            section.appendChild(para1);
-            }
-        }
+            para1.innerText = json.hits[i].recipe.label;
+            article.appendChild(para1);
 
-    };
+
+            let img = document.createElement('img');
+            img.src = json.hits[i].recipe.image;
+            article.appendChild(img);
+
+            let link = document.createElement('a');
+            link.href = json.hits[i].recipe.url;
+            link.appendChild(img);
+            article.appendChild(link);
+            article.className = "result" + i;
+            section.appendChild(article);
+
+            // let para2 = document.createElement('p');
+            // para2.innerText = json.hits[i].recipe.url;
+            // section.appendChild(para2);
+
+            
+
+            // let current = json.hit[i].recipe.url;
+            // link.href = current.json.hit[i].recipe.url;
+            // link.img = current.json.hit[i].recipe.url;
+
+            //let a = document.createElement('a');
+             //= json.hits[i].recipe.url;
+
+            // let para3 = document.createElement('p');
+            // para3.innerText = json.hits[i].recipe.calories;
+            // section.appendChild(para3);
+
+            // let para4 = document.createElement('p');
+            // para4.innerText = json.hits[i].recipe.ingredients[0].text;
+            // section.appendChild(para4);
+
+            
+
+
+            // let nutrients = document.createElement('nutrients');
+            //console.log(json.hints[i].food.nutrients.ENERC_KCAL);
+
+        }
+    }
+
+};
       //let nutrients = document.createElement('nutrients');
       //let food = document.createElement('food');
 
-//       console.log(json.parsed[""].food);
+//       console.log(json.hints[i].food.label);
 //       console.log(json.parsed[""].nutrients);
 
 //       let para1 = document.createElement('p');
@@ -68,8 +114,8 @@ function displayResults(json) {
 //       let para2 = document.createElement('p');
 //         para2.setAttribute("class","nutrients");
 
-//         para1.innerText = json.parsed[""].food;
-//         para2.innerText = json.parsed[""].nutrients;
+//         
+//         para2.innerText = json.hint[i].food.label.nutrients;
 
 //         
 //         section.appendChild(para2);
@@ -83,7 +129,7 @@ function displayResults(json) {
 //       //let nutrients = document.createElement('nutrients');
 //       //let food = document.createElement('food');
 //       //
-      
+
 
 //   food:
 // category: "Generic foods"
@@ -95,3 +141,6 @@ function displayResults(json) {
 // ENERC_KCAL: 406
 // FAT: 33.82
 // PROCNT: 24.04 
+//let label = document.createElement('label');
+            //label = json.hints[i].food.label;
+            //para1.setAttribute("class",label);
